bcf40fde6c1ad45fccae91e73e097cf0314d889f
Leo Dion <leogdion@brightdigit.com>
2022-11-20 20:40:56 -0500

    Added Gates

--- Harvester/ContentView.swift
+++ Harvester/ContentView.swift
@@ -7,26 +7,110 @@
 
 import SwiftUI
 
+struct Source {
+  let id : UUID
+}
+
+protocol GateOperation {
+  func compute (_ values: [Bool]) -> Bool
+}
+
+extension GateOperation {
+  func compute (_ values: [Bool?]) -> Bool? {
+    let compactValues = values.compactMap{$0}
+    guard compactValues.count == values.count else {
+      return nil
+    }
+    return self.compute(compactValues)
+  }
+}
+
+struct AndGateOp : GateOperation {
+  func compute(_ values: [Bool]) -> Bool {
+    values.allSatisfy{$0}
+  }
+}
+
+struct OpGate : Identifiable {
+  let id : UUID
+  var sources : [Source]
+  var operation : GateOperation
+  var offset : CGPoint = .init(x: 20, y: 20)
+  var imageName : String
+  var color : Color
+}
+
+struct OpGateValue : Identifiable {
+  var gate : OpGate
+  let value : Bool?
+  
+  var id: UUID {
+    return gate.id
+  }
+}
 class World : ObservableObject {
   @Published var buttons : [HVButton]
+  @Published var gates: [OpGate]
   
+  @Published var gateValues = [OpGateValue]()
   
   init () {
-    self.buttons = [HVButton(imageName: "007-cloud", color: .blue), HVButton(imageName: "017-fire", color: .orange)]
+    let buttons = [HVButton(imageName: "007-cloud", color: .blue), HVButton(imageName: "017-fire", color: .orange)]
+    self.buttons = buttons
+    
+    self.gates = [
+      OpGate(id: .init(), sources: buttons.map{
+      .init(id: $0.id)
+      }, operation: AndGateOp(), imageName: "008-coin", color: .red)]
+    
+    let buttonValueDictionary = self.$buttons.map{
+      $0.map{
+        ($0.id, $0.isOn)
+      }
+    }.map(Dictionary.init(uniqueKeysWithValues:))
+    
+    self.$gates.combineLatest(buttonValueDictionary).map { gates, values in
+      return gates.map { gate in
+        let values = gate.sources.map{
+          values[$0.id]
+        }
+        let value = gate.operation.compute(values)
+        return OpGateValue(gate: gate, value: value)
+      }
+    }.receive(on: DispatchQueue.main).assign(to: &self.$gateValues)
   }
   
-  func bindingForButton(withID id: UUID) -> Binding<HVButton> {
-    let index = self.buttons.firstIndex(where: {$0.id == id})
+  func bindingFor<CollectionType : MutableCollection, ObjectIdentifierType> (keyPath :
+                                                                      ReferenceWritableKeyPath<World, CollectionType>, withID id: ObjectIdentifierType) -> Binding<CollectionType.Element> where CollectionType.Element : Identifiable, CollectionType.Element.ID == ObjectIdentifierType  {
+    
+    let index = self[keyPath: keyPath].firstIndex(where: {$0.id == id})
     
     guard let index else {
       preconditionFailure()
     }
     
     return .init {
-      return self.buttons[index]
+      return self[keyPath: keyPath][index]
     } set: { button in
-      self.buttons[index] = button
+      self[keyPath: keyPath][index] = button
     }
+  }
+  
+  func bindingForButton(withID id: UUID) -> Binding<HVButton> {
+    let keyPath = \World.buttons
+    
+    return bindingFor(keyPath: keyPath, withID: id)
+//    let index = self[keyPath: keyPath].firstIndex(where: {$0.id == id})
+//
+//    guard let index else {
+//      preconditionFailure()
+//    }
+//
+//    return .init {
+//      return self[keyPath: keyPath][index]
+//    } set: { button in
+//      self[keyPath: keyPath][index] = button
+//    }
 
   }
 }
@@ -40,7 +124,10 @@ struct ContentView: View {
         let buttonBinding = self.world.bindingForButton(withID: button.id)
               HVButtonView(state: buttonBinding)
       }
-
+      
+      ForEach(self.world.gateValues) { gate in
+        HVGateView(state:  self.world.bindingFor(keyPath: \.gateValues, withID: gate.id))
+        }
     }
 }
 
---
+++ Harvester/HVGateView.swift
@@ -0,0 +1,42 @@
+//
+//  HVGateView.swift
+//  Harvester
+//
+//  Created by Leo Dion on 11/20/22.
+//
+
+import SwiftUI
+
+struct HVGateView: View {
+  @Binding var state : OpGateValue
+  var simpleDrag: some Gesture {
+    
+      DragGesture()
+          .onChanged { value in
+            self.state.gate.offset = value.location
+          }
+  }
+    var body: some View {
+      ZStack {
+        Image(self.state.gate.imageName).resizable().renderingMode(.template).aspectRatio( contentMode: .fit).offset(y: -10).scaleEffect(.init(width: 0.5, height: 0.5)).foregroundColor(state.gate.color)
+      }.frame(height: 80).opacity(state.value == true ? 1.0 : 0.5).position(state.gate.offset)
+            .gesture(
+              simpleDrag
+            )
+    }
+}
+
+struct ValueGateOperation : GateOperation {
+  let value : Bool
+  func compute(_ values: [Bool]) -> Bool {
+    return value
+  }
+}
+
+struct HVGateView_Previews: PreviewProvider {
+    static var previews: some View {
+      HVGateView(state:
+          .constant(.init(gate: .init(id: .init(), sources: [], operation: ValueGateOperation(value: true), imageName: "008-coin", color: .red), value: true))
+      )
+    }
+}
